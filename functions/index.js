0b4312c44aee281864e457f2116eded36df3f718
Taylor Niles <tniles320@gmail.com>
2021-04-20 16:18:39 -0700

    update

--- functions/index.js
+++ functions/index.js
@@ -18,8 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    // origin: "http://localhost:3000", // <-- location of the react app were connecting to
-    // credentials: true,
+    origin: "https://foothillfitness.com/" || "http://localhost:3000", // <-- location of the react app were connecting to
+    credentials: true,
   })
 );
 
